116d369445581d9911a1d1b2c8d8f2f78c3f42b0
Mike Pruett <mikepruett3@gmail.com>
2019-08-15 02:08:47 -0500

    back to this

--- backup.sh
+++ backup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#VOLUMES=(
-echo $CONTAINER
-docker inspect --format '{{json .Mounts}}' $CONTAINER | python -m json.tool
-#)
+VOLUMES=(docker inspect --format '{{json .Mounts}}' $CONTAINER | python -m json.tool)
 
-#echo $VOLUMES
+echo $VOLUMES
