26974bb42bee9a96fc2a0597ba22851d89d14bf1
Mike Pruett <mikepruett3@gmail.com>
2019-08-15 19:23:41 -0500

    doh!

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ Group=$(id -g -n)
 TimeStamp=$(date +"%m-%d-%Y_%H-%M-%S")
 
 # Load Appropriate Function Scripts based on $ContainerTech
-$ContainerTech="docker"
+ContainerTech="docker"
 
 function Load_Functions () {
     for fn in $(find functions/$ContainerTech/ -name "*.sh"); do
