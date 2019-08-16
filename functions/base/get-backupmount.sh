f6df56ba0f571069a4e45cc813d00cf004dd2476
Mike Pruett <mikepruett3@gmail.com>
2019-08-15 19:51:39 -0500

    needed the not (!)

--- functions/base/get-backupmount.sh
+++ functions/base/get-backupmount.sh
@@ -1,7 +1,7 @@
 function get-backupmount {
     echo ""
     read -p "Backup MountPoint? > " BackupMountPoint
-    if [ -d $BackupMountPoint ]; then
+    if [ ! [ -d $BackupMountPoint ] ]; then
         echo "$BackupMountPoint not a valid location!"
         exit 1
     fi
