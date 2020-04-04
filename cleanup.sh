cd2394b0e7aa3b889e7700e274b2fe261f303f54
Mike Pruett <mikepruett3@gmail.com>
2020-04-03 20:15:10 -0500

    Added cleanup.sh script

---
+++ cleanup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Variables
+ScriptBase=$(dirname $0)
+
+# Check Parameters
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -days)
+            shift;
+            Days=$1;
+            ;;
+        -b)
+            shift;
+            if [ -d $1 ]; then
+                BackupMount="$1/";
+            else
+                echo "$1 not a valid location!"
+                exit 1
+            fi
+            ;;
+        *)  ;;
+    esac
+    shift
+done
+
+# Delete Archive files older than $Days
+find "$BackupMount" -name "*.tar.gz" -mtime "+$Days" -print
